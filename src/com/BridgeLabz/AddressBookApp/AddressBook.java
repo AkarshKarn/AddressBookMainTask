798d2939dbd6b9315cf2481b5f5aa29d29be2b6d
AkarshKarn <akkikarn20@gmail.com>
2023-12-01 11:31:26 +0530

    [Akarsh] Added: Update Contact method added in the program to the UC3 Branch.

--- src/com/BridgeLabz/AddressBookApp/AddressBook.java
+++ src/com/BridgeLabz/AddressBookApp/AddressBook.java
@@ -42,13 +42,88 @@ public class AddressBook {
     }
 
 
+    public void updateContacts() {
+        boolean found = false;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter first Name:");
+
+        String firstName = sc.nextLine();
+
+
+        for (Contact contacts1 : cc) {
+            if (contacts1.getFirstName().equals(contacts1.firstName)) {
+                System.out.println("Enter the new First Name:");
+                String firstName1 = sc.nextLine();
+
+                System.out.println("Enter the new Last Name:");
+                String lastName = sc.nextLine();
+
+                System.out.println("Enter the new Address:");
+                String address = sc.nextLine();
+
+                System.out.println("Enter the new City Name:");
+                String city = sc.nextLine();
+
+                System.out.println("Enter the new State:");
+                String state = sc.nextLine();
+
+                System.out.println("Enter the new zipcode:");
+                int zipcode = sc.nextInt();
+
+                System.out.println("Enter the new Phone Number:");
+                long phoneNo = sc.nextLong();
+
+                System.out.println("Enter the new email:");
+                String email = sc.nextLine();
+
+                contacts1.setFirstName(firstName1);
+                contacts1.setLastName(lastName);
+                contacts1.setAddress(address);
+                contacts1.setCity(city);
+                contacts1.setState(state);
+                contacts1.setZipcode(zipcode);
+                contacts1.setPhoneNo(phoneNo);
+                contacts1.setEmail(email);
+
+                System.out.println(contacts1);
+                System.out.println("Contacts updated");
+                found = true;
+            }
+        }
+        if (!found) {
+            System.out.println("Contact details is not present in the directory");
+        } else {
+            System.out.println("Contacts details updated  successfully");
+        }
+    }
+
+
+    static boolean flow=true;
     public static void main(String[] args) {
-        System.out.println("Welcome to Address Book Program:");
+
         AddressBook addressBook = new AddressBook();
         Scanner sc = new Scanner(System.in);
 
-        addressBook.addContact();
+        do {
+            System.out.println("Welcome to the Address Book Program"
+                    + "\n1. Add Contact Details"
+                    + "\n2. Update Contact Details.");
+
+            System.out.println("Enter the choice:");
+            int option = sc.nextInt();
+            switch (option) {
+                case 1:
+                    System.out.println("Add Contacts:");
+                    addressBook.addContact();
+                    break;
+
+                case 2:
+                    System.out.println("Update Contacts");
+                    addressBook.updateContacts();
 
 
+            }
+        }while (flow) ;
     }
 }
+
